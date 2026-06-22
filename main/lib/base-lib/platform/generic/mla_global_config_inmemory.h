3e876b2e712a7a7b9605414358ed423fc398de24
Christian Schlegel <christian.schlegel@outlook.com>
2026-06-22 19:54:16 +0200

    Refactor platform includes and update function names for consistency

--- main/lib/base-lib/platform/generic/mla_global_config_inmemory.h
+++ main/lib/base-lib/platform/generic/mla_global_config_inmemory.h
@@ -8,7 +8,7 @@
 // In-memory configuration storage
 #include "../../core/config/mla_config.h"
 
-#if !defined(mla_max_config_size)
+#ifndef mla_max_config_size
 #define mla_max_config_size (4 * 1024) // 4KB is default
 #endif
 
@@ -16,7 +16,7 @@ static mla_byte_t g_config_storage[mla_max_config_size];
 static mla_size_t g_used_config_size = 0;
 
 
-mla_bytes_t __generic_inmemory_read_config_input() {
+mla_bytes_t mla_private_generic_inmemory_read_config_input() {
 
     if (g_used_config_size == 0) {
         return mla_bytes_empty();
@@ -25,12 +25,12 @@ mla_bytes_t __generic_inmemory_read_config_input() {
     return  mla_bytes_from_external_buffer(mla_platform_pointer_to_managed_pointer(g_config_storage), g_used_config_size);
 }
 
-mla_bytes_t __generic_inmemory_create_config_output_buffer() {
+mla_bytes_t mla_private_generic_inmemory_create_config_output_buffer() {
 
     return mla_bytes(mla_max_config_size);
 }
 
-mla_bool_t __generic_inmemory_commit_config_output(mla_bytes_t& output, mla_size_t unused_bytes) {
+mla_bool_t mla_private_generic_inmemory_commit_config_output(mla_bytes_t& output, mla_size_t unused_bytes) {
 
     if (unused_bytes > mla_max_config_size) {
         return false;
@@ -41,7 +41,7 @@ mla_bool_t __generic_inmemory_commit_config_output(mla_bytes_t& output, mla_size
     return true;
 }
 
-mla_bool_t __generic_inmemory_config_reset() {
+mla_bool_t mla_private_generic_inmemory_config_reset() {
 
     mla_memset(g_config_storage, 0, mla_max_config_size);
     g_used_config_size = 0;
@@ -49,10 +49,10 @@ mla_bool_t __generic_inmemory_config_reset() {
 }
 
 mla_config_low_level_operations_t g_config_low_level_operations = {
-    __generic_inmemory_read_config_input,
-    __generic_inmemory_create_config_output_buffer,
-    __generic_inmemory_commit_config_output,
-    __generic_inmemory_config_reset
+    mla_private_generic_inmemory_read_config_input,
+    mla_private_generic_inmemory_create_config_output_buffer,
+    mla_private_generic_inmemory_commit_config_output,
+    mla_private_generic_inmemory_config_reset
 };
 
 #endif
